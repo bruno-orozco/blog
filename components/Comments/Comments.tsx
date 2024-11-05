df134d5486e771b6212f102c73c55d22623702bf
Bruno <orozco.bruno@comunidad.unam.mx>
2024-11-04 23:08:03 -0600

    first commit

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -3,6 +3,7 @@ import Giscus from "@giscus/react";
 import { useTheme } from "../../hooks/use-theme";
 import { styled } from "../../lib/styles/stitches.config";
 import { giscusConfig } from "../../lib/config";
+import { githubRepo } from "../../lib/config";
 import type { ComponentProps } from "react";
 import type { GiscusProps } from "@giscus/react";
 
@@ -22,16 +23,31 @@ const Comments = ({ title, ...rest }: CommentsProps) => {
 
   // TODO: use custom `<Loading />` spinner component during suspense
   return (
-    <Wrapper {...rest}>
-      <Giscus
-        {...(giscusConfig as GiscusProps)}
-        term={title}
-        mapping="specific"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        theme={activeTheme === "dark" ? activeTheme : "light"}
-      />
-    </Wrapper>
+    <div>
+      <Wrapper {...rest}>
+        <Giscus
+          {...(giscusConfig as GiscusProps)}
+          term={title}
+          mapping="specific"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          theme={activeTheme === "dark" ? activeTheme : "light"}
+        />
+      </Wrapper>
+      <Wrapper {...rest}>
+        <Giscus
+          repo="bruno-orozco/blog"
+          repoId={giscusConfig.repoId}
+          category={giscusConfig.category}
+          categoryId={giscusConfig.categoryId}
+          term={title}
+          mapping="specific"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          theme={activeTheme === "dark" ? activeTheme : "light"}
+        />
+      </Wrapper>
+    </div>
   );
 };
 
