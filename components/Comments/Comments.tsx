0cd043426f4be283ca650f50f7bb7554d74c54f4
borozco-horus <borozco@grupoabraxas.com>
2025-07-09 23:20:17 -0600

    fix: update frame comments

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -3,7 +3,6 @@ import Giscus from "@giscus/react";
 import { useTheme } from "../../hooks/use-theme";
 import { styled } from "../../lib/styles/stitches.config";
 import { giscusConfig } from "../../lib/config";
-import { githubRepo } from "../../lib/config";
 import type { ComponentProps } from "react";
 import type { GiscusProps } from "@giscus/react";
 
@@ -21,26 +20,23 @@ export type CommentsProps = ComponentProps<typeof Wrapper> & {
 const Comments = ({ title, ...rest }: CommentsProps) => {
   const { activeTheme } = useTheme();
 
-  // TODO: use custom `<Loading />` spinner component during suspense
   return (
-    <div>
-      <Wrapper {...rest}>
-        <Giscus
-          id="giscus-comments"
-          repo={githubRepo as `${string}/${string}`}
-          repoId={giscusConfig.repoId}
-          category={giscusConfig.category}
-          categoryId={giscusConfig.categoryId}
-          mapping="specific"
-          term={title}
-          strict="1"
-          reactionsEnabled="1"
-          emitMetadata="1"
-          lang="es"
-          theme={activeTheme === "dark" ? "dark" : "light"}
-        />
-      </Wrapper>
-    </div>
+    <Wrapper {...rest}>
+      <Giscus
+        id="giscus-comments"
+        repo={giscusConfig.repo}
+        repoId={giscusConfig.repoId}
+        category={giscusConfig.category}
+        categoryId={giscusConfig.categoryId}
+        mapping="specific"
+        term={title}
+        strict="1"
+        reactionsEnabled="1"
+        emitMetadata="1"
+        lang="es"
+        theme={activeTheme === "dark" ? "dark" : "light"}
+      />
+    </Wrapper>
   );
 };
 
